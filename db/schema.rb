a3db1cad8103ebe70729ebb3bcdd43cae097490b
Fabien Bessez <fabien.bessez@gmail.com>
2017-03-29 10:49:50 -0400

    cookies, session expirations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329131910) do
+ActiveRecord::Schema.define(version: 20170329143202) do
 
   create_table "calendars", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170329131910) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
