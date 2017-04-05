89ce092dd76ae4abfe7fcc9237c14ca4e8a69f71
Fabien Bessez <fabien.bessez@gmail.com>
2017-04-05 10:57:57 -0400

    Save before deploy to heroku

--- db/migrate/20170405145055_create_users.rb
+++ db/migrate/20170405145055_create_users.rb
@@ -9,5 +9,5 @@ class CreateUsers < ActiveRecord::Migration[5.0]
 	    t.datetime "created_at",       null: false
 	    t.datetime "updated_at",       null: false
 	  end
-	end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405034435) do
-
-  create_table "calendars", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170405145055) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
