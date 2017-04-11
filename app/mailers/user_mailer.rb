de4487d47622dd4eabf9b0af53e545a90ff4d70d
Fabien Bessez <fabien.bessez@gmail.com>
2017-04-11 17:02:30 -0400

    Sends emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
 	default from: 'b16fabien@gmail.com'
 # http://localhost:3000/rails/mailers/user_mailer/contact_user.txt
-	def contact_user(user)
+	def contact_user(user, post)
 		@user = user
-		mail(to: @user.email, subject:"This is an email")
+		@post = post
+		mail(to: @user.email, subject: @post.title)
 	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
       :address              => 'smtp.gmail.com',
       :port                 => 587,
       :domain               => 'gmail.com',
-      :user_name            => 'XXXXXXX@gmail.com',
+      :user_name            => 'XXXXXX@gmail.com',
       :password             => 'XXXXXXX',
       :authentication       => 'plain',
       :enable_starttls_auto => true
