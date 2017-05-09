59c59fc8346fc1bf61f962feacd91a4beaec3b70
Fabien Bessez <fabien.bessez@gmail.com>
2017-05-09 19:36:32 -0400

    prod email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,30 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+
+  # Emailing Things
+    config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.default_url_options = {
+    :host =>  "https://obscure-headland-11563.herokuapp.com/"
+  }
+
+  config.action_mailer.perform_deliveries = true
+ 
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'wesadmits@gmail.com',
+      :password             => 'kailabs2017',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+    }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
